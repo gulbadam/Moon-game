40a89589b5973a48e60ad608314cd81b3e324eaf
yelena guliyeva <gulbadam@gulbadam.com>
2018-10-09 18:02:37 -0700

    asteroid

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 (function() {
-    var canvas, ctx, w, h, player1X, player2X, playerY, radius, backImg, angle1, angle2, currIndex1, currIndex2, turn, team, moveStep, angleStep ;
+    var canvas, ctx, w, h, player1X, player2X, playerY, radius, backImg, angle1, angle2, currIndex1, currIndex2, turn, team, moveStep, angleStep, dirX, dirY, tempIndex1, tempIndex2, asteroidRadius ;
     function init() {
         console.log("start")
         canvas = document.getElementById('moonWarsCanvas');
@@ -15,6 +15,11 @@
         angle2 = Math.PI;
         currIndex1 = {x: 0, y: 0};
         currIndex2 = {x: 0, y: 0};
+        tempIndex1 ={x: -10, y: -10};
+        tempIndex2 = { x: -10, y: -10 };
+        asteroidRadius = 2;
+        dirX= 1;
+        dirY=1;
         turn = 0;
         team = "none";
         moveStep = 2;
@@ -31,6 +36,11 @@ ctx.drawImage(backImg, 0, 0, w, h)
 drawLayout();
 drawPlayer1(player1X, playerY, angle1);
 drawPlayer2(player2X, playerY, angle2);
+if(!turn && tempIndex1.x!=-10 && tempIndex1.y!=-10) {
+    launchAsteroid(tempIndex1);
+} else if (turn && tempIndex2.x != -10 && tempIndex2.y != -10){
+    launchAsteroid (tempIndex2);
+}
 }
 function addBackground() {
     console.log ('Hey yo!!');
@@ -73,6 +83,40 @@ function drawPlayer2(x, y, angle) {
     ctx.fillStyle = '#004cb3';
     ctx.fill();
     }
+     function launchAsteroid(index) {
+         if (index.x>=(w/2)-2 && index.x<=(w/2)+2 && index.y<=h && index.y>=h-100) {
+             dirX = -dirX;
+         } else if (index.x < 0) {
+             dirX = -dirX;
+         } else if (index.x > w) {
+             dirX = -dirX;
+        } else if ( index.y < 0) {
+            dirY = -dirY;
+        } else if (index.y > h) {
+            turn = 1- turn;
+            return;
+        }
+        if(!turn) {
+            tempIndex1.x = index.x + dirX*2*Math.cos(angle1);
+            tempIndex1.y = index.y + dirY * 2 * Math.sin(angle1)
+        } else{
+            tempIndex2.x = index.x + dirX * 2 * Math.cos(angle2);
+            tempIndex2.y = index.y + dirY * 2 * Math.sin(angle2)
+        }
+        ctx.beginPath();
+        if(!turn) {
+            ctx.arc(tempIndex1.x, tempIndex1.y, asteroidRadius, 0, 2*Math.PI, false )
+        }else{
+            ctx.arc(tempIndex2.x, tempIndex2.y, asteroidRadius, 0, 2 * Math.PI, false) 
+        }
+        ctx.fillStyle ="yellow";
+        ctx.fill();
+        setTimeout(() => {
+            addBackground()
+        }, 100);
+
+
+     }
     function keyEvent(e) {
         e.stopImmediatePropagation();
         var keyCode = e.which;
@@ -80,15 +124,70 @@ function drawPlayer2(x, y, angle) {
         switch (keyCode) {
             case 37 :
                 if(!turn) {
+                    console.log("running 37")
                     if(player1X - 40 > 0) {
                         player1X = player1X - moveStep;
-                    }
+                        console.log("1x +"+player1X)
+                        console.log ('-40 1x');
+                        }
                 } else {
                     if(player2X - 40 > (w/2)+2) {
                         player2X = player2X - moveStep;
+                        console.log ("2x -" + player2X);
+                        console.log ('-40 2x');
                     }
                 }
                 addBackground();
+                break;
+            case 39:
+                if (!turn) {
+                    console.log("running 39")
+                    if (player1X + 40 < (w/2)-2) {
+                        player1X = player1X + moveStep;
+                        console.log ("1x ++" + player1X);
+                        console.log("+40 1x")
+                    }
+                } else {
+                    if (player2X + 40 < w ) {
+                        player2X = player2X + moveStep;
+                        console.log("2x +" + player2X)
+                        console.log("+40 2x")
+                    }
+                }
+                addBackground();
+                break;
+            case 38:
+                if (!turn) {
+                    console.log("running 38")
+                    if (angle1 + angleStep < Math.PI) {
+                        angle1 = angle1 + angleStep;
+                        console.log("angle1 +" + angle1);
+                        }
+                } else {
+                    if (angle2 + angle2 < Math.PI) {
+                        angle2 = angle2 + angleStep;
+                        console.log("angle2" + angle2)
+                        }
+                }
+                addBackground();
+                break;
+            case 40:
+                if (!turn) {
+                    console.log("running 40")
+                    if (angle1 - angleStep > 0) {
+                        angle1 = angle1 - angleStep;
+                        console.log("angle1 +" + angle1);
+                    }
+                } else {
+                    if (angle2 - angle2 > 0) {
+                        angle2 = angle2 -  angleStep;
+                        console.log("angle2" + angle2)
+                    }
+                }
+                addBackground();
+                break;
+            case 32:
+            launchAsteroid(currIndex1);
                 break;
         
             default:
